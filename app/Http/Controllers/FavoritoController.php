8b0e051e370d60cac39495e8d52b441eae449cca
Laura2R <126925385+Laura2R@users.noreply.github.com>
2025-06-04 12:23:13 +0200

    Favoritos final

--- app/Http/Controllers/FavoritoController.php
+++ app/Http/Controllers/FavoritoController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Linea;
-use App\Models\Parada;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
--- database/migrations/2025_05_10_203934_create_favoritos_lineas_table.php
+++ database/migrations/2025_05_10_203934_create_favoritos_lineas_table.php
@@ -6,19 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('favoritos_lineas', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->unsignedBigInteger('id_linea'); // CAMBIO: de string a unsignedBigInteger
+            $table->string('id_linea');
             $table->foreign('id_linea')->references('id_linea')->on('lineas')->onDelete('cascade');
             $table->timestamps();
-
-            $table->unique(['user_id', 'id_linea']);
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('favoritos_lineas');
