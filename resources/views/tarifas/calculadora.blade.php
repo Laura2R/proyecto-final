cae10b309e715fdc9d5c7bacf22267ee7ae987fd
Laura2R <126925385+Laura2R@users.noreply.github.com>
2025-06-09 05:23:22 +0200

    Cambio icono

--- resources/views/tarifas/calculadora.blade.php
+++ resources/views/tarifas/calculadora.blade.php
@@ -283,10 +283,8 @@
             <!-- Información adicional -->
             <div class="bg-blue-50 border-l-4 border-blue-400 p-4 rounded">
                 <div class="flex">
-                    <div class="flex-shrink-0">
-                        <svg class="h-5 w-5 text-blue-400" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
+                    <div class="flex-shrink-0 text-blue-400">
+                        <i class="fa-solid fa-circle-info"></i>
                     </div>
                     <div class="ml-3">
                         <h4 class="text-blue-800 font-semibold"><i class="fa-regular fa-lightbulb"></i> Información</h4>
